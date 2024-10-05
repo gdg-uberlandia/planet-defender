2b288c01035080c22bd8581444a7da017158b583
Tacio Medeiros <taciomedeiros@gmail.com>
2024-10-05 16:50:19 -0300

    feat: running plannet

--- Assets/Planet/Scripts/Planet.cs
+++ Assets/Planet/Scripts/Planet.cs
@@ -7,12 +7,18 @@ public class Planet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        float h = Input.GetAxis("Horizontal");
+
+
+        float angle = Mathf.Atan2(directionToCenter.y, directionToCenter.x) * Mathf.Rad2Deg + 90;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
     }
 }
