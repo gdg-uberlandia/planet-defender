8709c900e81b4c9fdfbc568d75b8900f035b758f
Tacio Medeiros <taciomedeiros@gmail.com>
2024-10-29 23:36:15 -0300

    feat: adiciona menu

---
+++ Assets/Menu/Scripts/MenuController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    [SerializeField]
+    private string gameLevel = "Level1";
+
+    // Update is called once per frame
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameLevel);
+    }
+}
